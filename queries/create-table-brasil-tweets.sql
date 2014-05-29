7b4ab7a66ca9f628aa08a1028355355457b11fa4
Igo Brilhante <brilhanteigo@gmail.com>
2014-05-29 16:32:23 +0200

    Consulta para criar tabela de tweets no brasil

---
+++ queries/create-table-brasil-tweets.sql
@@ -0,0 +1,19 @@
+create table twitter.brasil_tweets_estado as
+select 
+  t.id as id ,
+  text,
+  latitude,
+  longitude,
+  retweeted,
+  id_tweet_str,
+  created_at,
+  in_reply_to_user_id,
+  lang,
+  place_name,
+  place_id,
+  user_id_str,
+  e.id as id_estado,
+  estado,
+  uf
+from brasil.estados e, twitter.brasil_geo_ref_tweets_all t
+where st_contains(e.the_geom, st_setsrid(st_makepoint(longitude, latitude), 4326)) = True
