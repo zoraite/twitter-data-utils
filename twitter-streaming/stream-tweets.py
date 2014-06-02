cca47d33f102b4fa45746bc0a7e2c1cffdf792d5
Igo Brilhante <brilhanteigo@gmail.com>
2014-06-02 17:07:17 +0200

    igo

--- twitter-streaming/stream-tweets.py
+++ twitter-streaming/stream-tweets.py
@@ -1,30 +1,73 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import datetime
+import json
+import ConfigParser
+import gzip
 
-# Go to http://dev.twitter.com and create an app.
-# The consumer key and secret will be generated for you after
-consumer_key=""
-consumer_secret=""
-
-# After the step above, you will be redirected to your app's page.
-# Create an access token under the the "Your access token" section
-access_token=""
-access_token_secret=""
 
 class StdOutListener(StreamListener):
-    """ A listener handles tweets are the received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
 
     """
+    A listener handles tweets are the received from the stream.
+    This is a basic listener that just prints received tweets to stdout.
+    """
+
+    def __init__(self):
+        super(StdOutListener, self).__init__()
+        self.prefix = "brasil-georef-tweets"
+        self.today = None
+        self.current_file = None
+
     def on_data(self, data):
+
+        if self.is_today():
+            self.write_data(data)
+        else:
+            self.close_file()
+            self.create_file()
+            self.write_data(data)
+
         print data
         return True
 
     def on_error(self, status):
         print status
 
+    def create_file(self):
+        self.today = datetime.date.today()
+        file_name = self.format_file_name()
+        self.current_file = gzip.open(file_name, "a")
+
+    def write_data(self, data):
+        self.current_file.write(json.dumps(data) + "\n")
+
+    def close_file(self):
+        if self.current_file is not None:
+            self.current_file.close()
+
+    def is_today(self):
+        today = datetime.date.today()
+        return today == self.today
+
+    def format_file_name(self):
+        return self.prefix + "-" + str(self.today.year) + str(self.today.month) + str(self.today.day) + ".json.gz"
+
+
 if __name__ == '__main__':
+
+    section = "API"
+
+    config = ConfigParser.RawConfigParser()
+    config.read('keys.api')
+
+    consumer_key = config.get(section, "consumer_key")
+    consumer_secret = config.get(section, "consumer_secret")
+
+    access_token = config.get(section, "access_token")
+    access_token_secret = config.get(section, "access_token_secret")
+
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
