3538c76545b6cf15ca1be15ded99e4e46312c15f
Igo Brilhante <brilhanteigo@gmail.com>
2014-06-12 11:48:07 +0200

    igo

--- twitter-streaming/brasil-stream-tweets.sh
+++ twitter-streaming/brasil-stream-tweets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # BRASIL
-python stream-tweets.py "brasil-georef-tweets" "-28.850,16.800,-73.817,-33.733"
+python stream-tweets.py "brasil-georef-tweets" "-73.817,-33.733,-28.850,16.800"
--- twitter-streaming/rio-stream-tweets.sh
+++ twitter-streaming/rio-stream-tweets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # RIO DE JANEIRO
-python stream-tweets.py "rio-georef-tweets" "-43.101261,-22.749060,-43.795799,-23.078119"
+python stream-tweets.py "rio-georef-tweets" "-43.795799,-23.078119,-43.101261,-22.749060"
